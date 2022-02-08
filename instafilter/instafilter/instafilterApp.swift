32a99531027653f4707c329daaac792f6521eea6
brightsummit.co <naseemoyebola@gmail.com>
2022-02-08 14:36:09 +0100

    initial commit

---
+++ instafilter/instafilter/instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instafilterApp.swift
+//  instafilter
+//
+//  Created by naseem on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
